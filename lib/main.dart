ca6344fa12e142651de967e6cb631adb0386496d
Master <isa.smail1123@gmail.com>
2022-12-19 17:07:15 +0600

    merge isa_UI

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
-      designSize: Size(375, 812),
+      designSize: const Size(375, 812),
       builder: (BuildContext context, Widget? child) => MaterialApp(
           title: 'Flutter Demo',
           theme: ThemeData(
